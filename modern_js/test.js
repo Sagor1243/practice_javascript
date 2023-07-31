c2ab01cabb3f64ca37a78c2cb962eae339836865
Sagor saha <sahasagor650@gmail.com>
2023-07-31 21:22:35 +0600

    Update test.js

--- modern_js/test.js
+++ modern_js/test.js
@@ -52,18 +52,121 @@
 
 
 
-const searchinput = document.querySelector(".search");
-const display = document.querySelector(".result");
-const thanks = document.querySelector(".thanks");
+// const searchinput = document.querySelector(".search");
+// const display = document.querySelector(".result");
+// const thanks = document.querySelector(".thanks");
 
 
 
- function show() {
-    display.innerHTML = this.value;
-    var self = this;
-    setTimeout(function() {
-        thanks.innerHTML = `you have typed ${self.value}`;
-    },1000)
- }
+//  function show() {
+//     display.innerHTML = this.value;
+//     var self = this;
+//     setTimeout(function() {
+//         thanks.innerHTML = `you have typed ${self.value}`;
+//     },1000)
+//  }
 
-searchinput.addEventListener("keyup",show);
+// searchinput.addEventListener("keyup",show);
+
+
+
+
+// var age = 11;
+
+
+// var type = age >= 18 ? "adult" : age < 10 ? "childd" : "young";
+
+
+// console.log(type);
+
+// var number = [1,2,3,4,5,6,7,8]
+
+// var result = number.find(function(currentvalue,currendIndex,arr) {
+//     return currentvalue > 5;
+// })
+// console.log(result);
+
+
+// class student{
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     test() {
+//         console.log("hello");
+//     }
+//     exampleFunction() {
+//         let array = [1,2,3];
+//         array.find(function() {
+//             this.test();
+//         },this);
+//     }
+    
+// }
+
+// var Student = new student("sagor",21);
+
+// Student.exampleFunction();
+
+// i = 0;
+// for(i = 0; i < 1000; i++) {
+//     console.log(`${i} puja valo na`);
+// }
+
+
+// var numbers = [1,2,3,4,5,6];
+
+
+// var result = numbers.findIndex((currentvalue,Index,arr) => {
+//     return currentvalue > 6
+// })
+
+// console.log(result);
+
+
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// var result = numbers.filter((currentvalue,index,arr) => {
+//     return currentvalue > 4;
+// })
+     
+
+// console.log(result);
+// console.log(numbers);
+
+
+// var numbers = [1,2,3,4,5]
+
+// var result = numbers.slice(1,3);
+
+// console.log(result);
+
+
+// var numbers = [1,2,3,4,5];
+
+// var result = numbers.splice(-1,3,10,11,12,13,14,15,16);
+
+
+// console.log(numbers);
+// console.log(result);
+
+
+// var numbers1 = [1,2,3,4,5];
+// var numbers2 = [1,2,3,4,5];
+// var numbers3 = [1,2,3,4,5];
+// var numbers4 = [1,2,3,4,5]
+
+// var result = numbers1.concat(numbers2,numbers3,numbers4);
+
+// console.log(result);
+
+
+var numbers = [1,2,3,4,5];
+
+numbers.push([7,8,9]);
+
+console.log(numbers);
+
+
+
+
