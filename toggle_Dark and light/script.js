0e937c963a34e76973ae9dc099c2a6d23502ed76
Sagor saha <sahasagor650@gmail.com>
2023-08-14 16:43:43 +0600

    dark/light mood

---
+++ toggle_Dark and light/script.js
@@ -0,0 +1,20 @@
+const toggle = document.getElementById('toggledark')
+const body = document.querySelector('body');
+
+toggle.addEventListener('click',function() {
+    this.classList.toggle("fa-moon");
+    if(this.classList.toggle('fa-solid-fa-lightbulb')){
+       body.style.background = 'white';
+       body.style.color = 'black';
+       body.style.transition = '2s'
+    } else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s'
+    }
+})
+
+
+ 
+
+ 
