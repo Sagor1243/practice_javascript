ba18f34524b87bf17fbfa8d30020ba4bd8c6e848
Sagor saha <sahasagor650@gmail.com>
2023-10-04 12:16:47 +0600

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const startbtn = document.querySelector('.start-btn')
 const popupinfo = document.querySelector('.popuop-info')
 const exitbtn = document.querySelector('.exit-btn')
-const main = document.querySelector('.main')
+const main = document.querySelector('.main');
+const quizSection = document.querySelector('quiz-section')
+const continueBtn = document.querySelector('continue-btn')
   
 startbtn.onclick = () => {
     popupinfo.classList.add('active')
@@ -12,3 +14,8 @@ exitbtn.onclick = () => {
     popupinfo.classList.remove('active')
     main.classList.remove('active')
 } 
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupinfo.classList.remove('active')
+    main.classList.remove('active')
+}
